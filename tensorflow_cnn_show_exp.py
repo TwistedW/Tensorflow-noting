e1167cd6369179c96e165edcc77ad1811f3802d0
TwistedW <2359375192@qq.com>
2017-09-10 09:58:41 +0800

    updatting

---
+++ tensorflow_cnn_show_exp.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib.pyplot as plt
+
+# 导入数字集0-9
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+N_TEST_IMG = 10
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
+    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
+    return result
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
+
+def conv2d(x, W):
+    # stride [1, x_movement, y_movement, 1]
+    #必要设置strides[0] = strides[3] = 1
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    # stride [1, x_movement, y_movement, 1]
+    return tf.nn.max_pool(x, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
+
+#定义placeholder用于数据的输入
+xs = tf.placeholder(tf.float32, [None, 784])/255.   # 数字集为28x28大小，再将其归一化
+ys = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32) #用于处理过拟合
+x_image = tf.reshape(xs, [-1, 28, 28, 1])
+# print(x_image.shape)  # [n_samples, 28,28,1]
+
+## conv1 layer ##
+W_conv1 = weight_variable([5, 5, 1, 32]) # patch 5x5, in size 1, out size 32
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
+h_pool1 = max_pool_2x2(h_conv1)                          # output size 14x14x32
+
+## conv2 layer ##
+W_conv2 = weight_variable([5,5, 32, 64]) # patch 5x5, in size 32, out size 64
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv2)                          # output size 7x7x64
+
+## fc1 layer ##
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+
+# [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+## fc2 layer ##
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+
+#计算真实数据与预测数据的误差大小
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction), reduction_indices=[1]))# loss
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+sess = tf.Session()
+init = tf.global_variables_initializer()
+sess.run(init)
+
+# initialize figure
+f, a = plt.subplots(2, N_TEST_IMG, figsize=(10, 2))
+plt.ion()   #为了在图像上持续显示
+
+# 初始原始数据显示
+view_data = mnist.test.images[:N_TEST_IMG]
+for i in range(N_TEST_IMG):
+    a[0][i].imshow(np.reshape(view_data[i], (28, 28)), cmap='gray')
+    a[0][i].set_xticks(())
+    a[0][i].set_yticks(())
+
+
+for step in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(10)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
+    if step % 100 == 0:
+        results = sess.run(prediction, feed_dict={xs: view_data, keep_prob: 0.5})
+        print(compute_accuracy(mnist.test.images[:10], mnist.test.labels[:10]))
+        for i in range(N_TEST_IMG):
+            a[1][i].clear()
+            a[1][i].imshow(np.reshape(results[i], (10, 1)), cmap='gray')
+            a[1][i].set_xticks(())
+            a[1][i].set_yticks(())
+        plt.draw()
+        plt.pause(0.01)
+plt.show()
