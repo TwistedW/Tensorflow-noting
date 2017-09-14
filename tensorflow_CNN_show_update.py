da5d7ce1e6420384a56689ac0548ab8ef4db7b53
TwistedW <2359375192@qq.com>
2017-09-14 15:39:33 +0800

    updatting

--- tensorflow_CNN_show_update.py
+++ tensorflow_CNN_show_update.py
@@ -16,9 +16,9 @@ test_y = mnist.test.labels[:2000]
 # plot one example
 print(mnist.train.images.shape)     # (55000, 28 * 28)
 print(mnist.train.labels.shape)   # (55000, 10)
-plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
-plt.title('%i' % np.argmax(mnist.train.labels[0]))
-plt.show()
+# plt.imshow(mnist.train.images[0].reshape((28, 28)), cmap='gray')
+# plt.title('%i' % np.argmax(mnist.train.labels[0]))
+# plt.show()
 
 tf_x = tf.placeholder(tf.float32, [None, 28*28])
 image = tf.reshape(tf_x, [-1, 28, 28, 1])              # (batch, height, width, channel)
