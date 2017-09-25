06c6806b946b38783cdd555ee3989af86ce9a4d2
TwistedW <2359375192@qq.com>
2017-09-25 16:54:12 +0800

    updatting

--- tensorflow_mnist/mnist_4.0_batchnorm_five_layers_sigmoid.py
+++ tensorflow_mnist/mnist_4.0_batchnorm_five_layers_sigmoid.py
@@ -1,18 +1,4 @@
 # encoding: UTF-8
-# Copyright 2016 Google.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import tensorflow as tf
 import tensorflow_mnist.tensorflowvisu
 import math
@@ -88,10 +74,12 @@ B5 = tf.Variable(tf.zeros([Q]))
 
 # Steady 98.5% accuracy using these parameters:
 # moving average decay: 0.998 (equivalent to averaging over two epochs)
-# learning rate decay from 0.03 to 0.0001 speed 1000 => max 98.59 at 6500 iterations, 98.54 at 10K it,  98% at 1300it, 98.5% at 3200it
+# learning rate decay from 0.03 to 0.0001 speed 1000 => max 98.59 at 6500 iterations, 98.54 at 10K it,
+# 98% at 1300it, 98.5% at 3200it
 
 def batchnorm(Ylogits, Offset, Scale, is_test, iteration):
-    exp_moving_avg = tf.train.ExponentialMovingAverage(0.998, iteration) # adding the iteration prevents from averaging across non-existing iterations
+    # adding the iteration prevents from averaging across non-existing iterations
+    exp_moving_avg = tf.train.ExponentialMovingAverage(0.998, iteration)
     bnepsilon = 1e-5
     mean, variance = tf.nn.moments(Ylogits, [0])
     update_moving_everages = exp_moving_avg.apply([mean, variance])
